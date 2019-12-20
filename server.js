d1cc1d2144dabcb36510cc357d58013c8d5aa2c3
Christian Lehr <christian_lehr.1@gmx.de>
2019-12-20 14:45:02 +0100

    initialize project

---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Christian',
+            email: 'christian_lehr@outlook.de',
+            password: '4444',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if (correctCredentials(req)) {
+        res.json("success");
+    }
+    if (wrongCredentials(req)) {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1])
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    });
+
+    if (!found) {
+        return res.status(404).json("not found");
+    }
+})
+
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+
+    if (!found) {
+        return res.status(404).json("not found");
+    }
+})
+
+app.listen('3000', () => {
+    console.log('app is running on port 3000');
+})
+
+function correctCredentials(req) {
+    const request = req.body;
+    const firstUser = database.users[0];
+
+    return request.email === firstUser.email
+        && request.password === firstUser.password;
+}
+
+function wrongCredentials(req) {
+    return !correctCredentials(req);
+}
+

391f70a93c452ec3749b714519af73c8096ba809
clehr <christian_lehr@outlook.de>
2020-01-10 12:10:38 +0100

    get image entry count from database

--- server.js
+++ server.js
@@ -90,18 +90,13 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    });
-
-    if (!found) {
-        return res.status(404).json("not found");
-    }
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get entries'));
 })
 
 

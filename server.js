4490d72287b07b7f7f1ad1e8ef880bdbc8496417
clehr <christian_lehr@outlook.de>
2020-01-10 12:25:46 +0100

    insert login hash when registering

--- server.js
+++ server.js
@@ -58,17 +58,30 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    const { email, name } = req.body;
-
-    return db('users')
-        .returning('*')
-        .insert({
-            name: name,
-            email: email,
-            joined: new Date()
-        }).then(users => {
-            res.json(users[0])
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: name,
+                        email: loginEmail[0],
+                        joined: new Date()
+                    })
+                    .then(users => {
+                        res.json(users[0])
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
         .catch(err => res.status(400).json('unable to register'))
 })
 

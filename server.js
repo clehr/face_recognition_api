7389ddd2c7a5b48d0ef387fa7a86b8b5c6b7c58e
clehr <christian_lehr@outlook.de>
2020-01-10 12:03:07 +0100

    return user from database

--- server.js
+++ server.js
@@ -75,16 +75,16 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    });
 
-    if (!found) {
-        return res.status(404).json("not found");
-    }
+    db.select().from('users')
+        .where({ 'id': id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            }
+            res.status(404).json('Not found');
+        })
+        .catch(err => res.status(400).json('error getting user'));
 })
 
 

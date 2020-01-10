228b9c31da46894ee71f934b15bcec238e50d2b9
clehr <christian_lehr@outlook.de>
2020-01-10 12:38:10 +0100

    handle signin from db

--- server.js
+++ server.js
@@ -48,13 +48,20 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-
-    if (correctCredentials(req)) {
-        res.json(database.users[0]);
-    }
-    if (wrongCredentials(req)) {
-        res.status(400).json('error logging in');
-    }
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select().from('users').where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            }
+            res.status(400).json("wrong credentials");
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
